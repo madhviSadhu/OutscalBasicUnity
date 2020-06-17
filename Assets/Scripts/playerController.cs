f6805a6f0a689c2482da0f2ce3cc7c84f5903180
madhviSadhu <39146979+madhviSadhu@users.noreply.github.com>
2020-06-17 15:55:38 +0530

    Chapter 3 Assignment 1

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    public class playerController : MonoBehaviour
+    {
+        private Animator animatorPlayer;
+        private float speed;
+        private Vector3 scale;
+
+        private void Start()
+        {
+            scale = transform.localScale;
+            animatorPlayer = GetComponent<Animator>();
+        }
+        private void Update()
+        {
+            speed = Input.GetAxisRaw("Horizontal");
+            animatorPlayer.SetFloat("Speed", Mathf.Abs(speed));
+            if (speed < 0)
+            {
+                scale.x = -1f * Mathf.Abs(scale.x);
+                transform.localScale = scale;
+            }
+            else if (speed > 0)
+            {
+                scale.x = Mathf.Abs(scale.x);
+                transform.localScale = scale;
+            }
+        }
+    }
+}
