1868550299a4d552fd3a08cbf40707825d554b9f
madhviSadhu <39146979+madhviSadhu@users.noreply.github.com>
2020-06-20 12:01:24 +0530

    Assignment 1  Vertical input & Jump

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -5,44 +5,78 @@ namespace Player
     public class playerController : MonoBehaviour
     {
         private Animator animatorPlayer;
-        private float speed;
+        private new BoxCollider2D collider;
+        private new Rigidbody2D rigidbody;
+        private bool isCrouch;
         private Vector3 scale;
-        private BoxCollider2D collider;
+        public Vector2 minColliderOffset, minColliderSize, maxColliderOffset , maxColliderSize;
+        public float speed , Jump;
 
         private void Start()
         {
             scale = transform.localScale;
             animatorPlayer = GetComponent<Animator>();
+            rigidbody = GetComponent<Rigidbody2D>();
             collider = GetComponent<BoxCollider2D>();
         }
         private void Update()
         {
-            speed = Input.GetAxisRaw("Horizontal");
-            animatorPlayer.SetFloat("Speed", Mathf.Abs(speed));
-            if (speed < 0)
+            float horizontalInput = Input.GetAxisRaw("Horizontal");
+            float verticalInput = Input.GetAxisRaw("Jump");
+            PlayerMove(horizontalInput , verticalInput);
+            PlayerAnimation(horizontalInput, verticalInput);
+        }
+        private void PlayerAnimation(float horizontal , float vertical)
+        {
+            animatorPlayer.SetFloat("Speed", Mathf.Abs(horizontal));
+            if (horizontal < 0)
             {
                 scale.x = -1f * Mathf.Abs(scale.x);
                 transform.localScale = scale;
             }
-            else if (speed > 0)
+            else if (horizontal > 0)
             {
                 scale.x = Mathf.Abs(scale.x);
                 transform.localScale = scale;
             }
-
+            if (vertical > 0)
+            {
+                animatorPlayer.SetBool("Jump", true);
+            }
+            else
+            {
+                animatorPlayer.SetBool("Jump", false);
+            }
+        }
+        private void PlayerMove(float horizontal, float vertical)
+        {
             if (Input.GetKeyDown(KeyCode.LeftControl))
             {
                 //play corch animation
-                collider.offset = new Vector2(-0.05f , 0.5f);
-                collider.size = new Vector2(0.7f, 1.2f);
+                isCrouch = true;
+                collider.offset = minColliderOffset;
+                collider.size = minColliderSize;
                 animatorPlayer.SetBool("Crouch", true);
             }
-            else if (Input.GetKeyUp(KeyCode.LeftControl))
+            else if(Input.GetKeyUp(KeyCode.LeftControl))
             {
-                //stop corch animation
-                collider.offset = new Vector2(0f, 1f);
-                collider.size = new Vector2(0.5f, 2f);
+                collider.offset = maxColliderOffset;
+                collider.size = maxColliderSize;
                 animatorPlayer.SetBool("Crouch", false);
+                isCrouch = false;
+            }
+            else if (vertical > 0)
+            {
+               rigidbody.AddForce(Vector2.up * Jump, ForceMode2D.Force);
+            }
+            else if (horizontal > 0 || horizontal < 0)
+            {
+                if(!isCrouch)
+                {
+                  Vector3 position = transform.position;
+                  position.x += horizontal * speed * Time.deltaTime;
+                  transform.position = position;
+                }
             }
         }
     }
